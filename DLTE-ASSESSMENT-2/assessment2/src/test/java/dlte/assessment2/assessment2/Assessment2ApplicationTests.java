d49cf57f9961afc066054d89995c0c5211a267ab
poojapshetty <125381734+poojapshetty@users.noreply.github.com>
2023-04-03 05:02:46 -0700

    Add files via upload

---
+++ DLTE-ASSESSMENT-2/assessment2/src/test/java/dlte/assessment2/assessment2/Assessment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dlte.assessment2.assessment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assessment2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
